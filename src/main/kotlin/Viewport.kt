4547b326bbfc239eaa3214fa6b9cbbb0dd2ab415
xibalbam <maelporret@outlook.fr>
2024-04-13 13:28:36 +0200

    feat: add camera

--- src/main/kotlin/Viewport.kt
+++ src/main/kotlin/Viewport.kt
@@ -1,6 +1,8 @@
 package fr.xibalba.renderer
 
 import fr.xibalba.math.*
+import fr.xibalba.renderer.events.EngineEvents
+import fr.xibalba.renderer.utils.lookAt
 
 object Viewport {
     private val camera = Camera(vec3(0f, 0f, 3f), vec3(0f, 0f, 0f))
@@ -12,5 +14,27 @@ object Viewport {
     fun setLogLevel(logLevel: Int): Viewport {
         Engine.logLevel = logLevel
         return this
+    }
+
+    @EventListener
+    fun addCameraToUBO(event: EngineEvents.CreateUniformBufferObject) {
+        event.uniformBufferObject.view = camera.getViewMatrix()
+    }
+}
+
+class Camera(var position: Vec3f, var rotation: Vec3f) {
+    fun move(direction: Vec3f) {
+        position += direction
+    }
+
+    fun rotate(direction: Vec3f) {
+        rotation += direction
+    }
+
+    fun getViewMatrix(): SquareMatrix<Float> {
+        val eye = position
+        val center = position + rotation
+        val up = vec3(0f, 1f, 0f)
+        return lookAt(eye, center, up)
     }
 }
